7074f0d36b0b6d41dbb82cdda45df136ba308977
mroxso <24775431+mroxso@users.noreply.github.com>
2026-02-04 20:30:56 +0100

    Change base path for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' && process.env.GITHUB_ACTIONS ? '/zelo-news/' : '/',
+  base: mode === 'production' && process.env.GITHUB_ACTIONS ? '/chat/' : '/',
   server: {
     host: "::",
     port: 8080,
